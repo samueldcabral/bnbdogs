d6a6dc43d38000de0ca86998009463fd2b5f6066
samueldcabral <samueldcabral@gmail.com>
2019-08-13 04:54:36 -0300

    first steps into creating a booking via frontend. Tested first booking and it worked ok. A few more implementations on the frontend part

--- src/components/HomeBox.vue
+++ src/components/HomeBox.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="bg-white p-5">
+    <h3>Hey, Who's a good boy?</h3>
+    <p>Book your room now</p>
+    <br />
     <date-picker v-model="time1" range appendToBody :lang="lang" format="YYYY-MM-DD" confirm></date-picker>
-    <p>{{time1}}</p>
+    <br />
+    <b-button variant="primary" @click="postBooking" class="button">Book now!</b-button>
   </div>
 </template>
 
 <script>
+import { addBooking } from "../services/services";
 import DatePicker from "vue2-datepicker";
 export default {
   components: { DatePicker },
   data() {
     return {
       time1: "",
+      dayprice: 666,
+      bookingDate: "",
+      dogId: 1,
       lang: {
         days: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
         months: [
@@ -35,8 +43,52 @@ export default {
       }
     };
   },
-  methods: {}
+  methods: {
+    postBooking() {
+      this.bookingDate = new Date(Date.now());
+      let duration = new Date(this.time1["1"] - this.time1["0"]).getDate();
+
+      let booking = {
+        booking_date: this.getDate(this.bookingDate),
+        "check-in_date": this.getDate(this.time1["0"]),
+        "check-out_date": this.getDate(this.time1["1"]),
+        duration,
+        day_price: this.dayprice,
+        dog_id: this.dogId
+      };
+
+      console.log(booking);
+
+      addBooking(booking).then(res => {
+        if (res.data.message === "Booking created") {
+          console.log(res.data.message);
+          alert("Booking created");
+
+          setTimeout(() => {
+            this.alertMessage = "";
+            // this.$router.push("/show-dogs")
+          }, 1000);
+        }
+        // verificar quando da erro
+        alert("Booking not created");
+        setTimeout(() => {
+          this.alertMessage = "";
+        }, 1000);
+      });
+    },
+    getDate(dateSent) {
+      return `${dateSent.getFullYear()}-${dateSent.getMonth() +
+        1}-${dateSent.getDate()}`;
+    }
+  }
 };
 </script>
 <style scoped>
+div {
+  width: 50%;
+  border-radius: 6px;
+}
+.button {
+  margin-top: 10px;
+}
 </style>
--- src/services/services.js
+++ src/services/services.js
@@ -1,78 +1,64 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://localhost:8080/api';
-
+const API_URL = "http://localhost:8080/api";
+// const API_URL = "http://localhost:9000/api";
 
 export function getUsers() {
-  const request = axios
-    .get(API_URL + '/user')
-    .then(res => res.data);
+  const request = axios.get(API_URL + "/user").then(res => res.data);
   return request;
 }
 
 export function getDogs() {
-  const request = axios
-    .get(API_URL + '/dog')
-    .then(res => res.data);
+  const request = axios.get(API_URL + "/dog").then(res => res.data);
   return request;
 }
 
 export function getServices() {
-  const request = axios
-    .get(API_URL + '/service')
-    .then(res => res.data);
+  const request = axios.get(API_URL + "/service").then(res => res.data);
   return request;
 }
 
 export function getBookings() {
-  const request = axios
-    .get(API_URL + '/booking')
-    .then(res => res.data);
+  const request = axios.get(API_URL + "/booking").then(res => res.data);
   return request;
 }
 
 export function addDog(dog) {
-  const request = axios
-    .post(API_URL+ '/dog', dog);
+  const request = axios.post(API_URL + "/dog", dog);
   return request;
 }
 
 export function deleteDog(dogId) {
-  const request = axios
-    .delete(`${API_URL}/dog/${dogId}`).then(res => res.data);
+  const request = axios.delete(`${API_URL}/dog/${dogId}`).then(res => res.data);
   return request;
 }
 
 export function editDog(dog) {
-  const request = axios
-    .put(`${API_URL}/dog/${dog.id}`, dog)
-  return request
+  const request = axios.put(`${API_URL}/dog/${dog.id}`, dog);
+  return request;
 }
 
 export function showDogsUser(userId) {
-  const request = axios
-    .get(`${API_URL}/dogs/${userId}`)
+  const request = axios.get(`${API_URL}/dogs/${userId}`);
   return request;
 }
 
 export function showBookingsUser(userId) {
-  const request = axios
-    .get(`${API_URL}/bookings/${userId}`)
+  const request = axios.get(`${API_URL}/bookings/${userId}`);
   return request;
 }
 
 export function findDogByID(dogId) {
-  const request = axios
-    .get(`${API_URL}/dog/${dogId}`)
-    .then(res => res.data);
+  const request = axios.get(`${API_URL}/dog/${dogId}`).then(res => res.data);
   return request;
 }
 
 export function addServiceBooking(bookingId, service) {
-  const request = axios
-    .post(`${API_URL}/booking/${bookingId}`, service)
+  const request = axios.post(`${API_URL}/booking/${bookingId}`, service);
   return request;
 }
 
-
-
+export function addBooking(booking) {
+  const request = axios.post(API_URL + "/booking", booking);
+  return request;
+}
