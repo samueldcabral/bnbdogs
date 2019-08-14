8bf15757131c4f512d81171d53c6db3535b3af81
Renatha <renatha.nvictor@gmail.com>
2019-08-13 22:15:41 -0300

    algumas modificacoes

---
+++ src/views/Registry.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <navBar />
+    <div class="container">
+      <RegBox />
+    </div>
+  </div>
+</template>
+
+
+<script>
+import NavBar from "../components/NavBar";
+import RegBox from "../components/RegistryBox";
+
+export default {
+  name: "registry",
+  components: { NavBar, RegBox }
+};
+</script>
+
+<style>
+body {
+  background: url("../../static/happydog.jpg") no-repeat !important;
+  background-size: cover !important;
+  overflow: hidden;
+}
+</style>
